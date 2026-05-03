76de146776a3060af2d9be3fe160e54b7a356283
prasanna14200 <150162583+prasanna14200@users.noreply.github.com>
2026-05-03 15:36:54 +0530

    Update

--- frontend/src/components/BannerProduct.js
+++ frontend/src/components/BannerProduct.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import image1 from '../assest/banner/img1.webp'
-import image2 from '../assest/banner/img2.webp'
-import image3 from '../assest/banner/img3.jpg'
-import image4 from '../assest/banner/img4.jpg'
-import image5 from '../assest/banner/img5.webp'
+import image1 from '../assets/banner/img1.webp'
+import image2 from '../assets/banner/img2.webp'
+import image3 from '../assets/banner/img3.jpg'
+import image4 from '../assets/banner/img4.jpg'
+import image5 from '../assets/banner/img5.webp'
 
 
-import image1Mobile from '../assest/banner/img1_mobile.jpg'
-import image2Mobile from '../assest/banner/img2_mobile.webp'
-import image3Mobile from '../assest/banner/img3_mobile.jpg'
-import image4Mobile from '../assest/banner/img4_mobile.jpg'
-import image5Mobile from '../assest/banner/img5_mobile.png'
+import image1Mobile from '../assets/banner/img1_mobile.jpg'
+import image2Mobile from '../assets/banner/img2_mobile.webp'
+import image3Mobile from '../assets/banner/img3_mobile.jpg'
+import image4Mobile from '../assets/banner/img4_mobile.jpg'
+import image5Mobile from '../assets/banner/img5_mobile.png'
 
 import { FaAngleRight } from "react-icons/fa6";
 import { FaAngleLeft } from "react-icons/fa6";
